4a0b94ff4ac2da2b05621e6d6338a0c0b318c6ac
Nakarin Hansawattana <nakarinh14@gmail.com>
2022-07-14 15:38:10 +0700

    Add OpenTelemetry automatic instrumentation

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@ set -e
 
 python manage.py migrate
 
-gunicorn --bind=0.0.0.0 --timeout 600 order_django.wsgi
+opentelemetry-instrument gunicorn order_django.wsgi -c gunicorn.config.py --bind=0.0.0.0 --reload --timeout 600
